f0e419587baf9111d8301d687006137acf7aec45
irohanrajput <itusharsingh27@gmail.com>
2023-09-16 19:39:04 +0530

    created models, added data for airports and flights, etc

--- flights/models.py
+++ flights/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
+class Airport(models.Model):
+    code = models.CharField(("Airport Code "), max_length=3)
+    city = models.CharField(("City Name"), max_length=50)
+
+    def __str__(self):
+        return f"{self.city}-{self.code}"
+
 class Flight(models.Model):
-    origin = models.CharField(("origin of the flight"), max_length=50)
-    destination = models.CharField(("destination of the flight"), max_length=50)
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField(("duratin of flight in mintues"))
+    
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination}" 
