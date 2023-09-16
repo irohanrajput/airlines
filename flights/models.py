5da1372488b7148096df8731129beabee1d0c611
irohanrajput <itusharsingh27@gmail.com>
2023-09-16 10:29:06 +0530

    started working on models and flights details

--- flights/models.py
+++ flights/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Flight(models.Model):
+    origin = models.CharField(("origin of the flight"), max_length=50)
+    destination = models.CharField(("destination of the flight"), max_length=50)
+    duration = models.IntegerField(("duratin of flight in mintues"))
