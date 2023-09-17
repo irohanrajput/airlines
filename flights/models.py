770df376646ff40dbec64eed113f8f1cd1c5d57d
Tushar Singh <itusharsingh27@gmail.com>
2023-09-17 17:42:33 +0530

    Update models.py

--- flights/models.py
+++ flights/models.py
@@ -5,7 +5,7 @@ class Airport(models.Model):
     city = models.CharField(("City Name"), max_length=50)
 
     def __str__(self):
-        return f"{self.city}-{self.code}"
+        return f"{self.city} {self.code}"
 
 class Flight(models.Model):
     origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
