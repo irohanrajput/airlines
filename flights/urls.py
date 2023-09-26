6ee01dce2a190c37f5268f702d5505bf06e35e16
irohanrajput <itusharsingh27@gmail.com>
2023-09-27 04:27:55 +0530

    added a flight booking section from the page where flight details are listed. using this booking section, we can book flight for a particular passenger provided the passenger must alreay exist in the database

--- flights/urls.py
+++ flights/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("<int:flight_id>", views.flight, name="flight"),
     # here, we're puttin "localhost/flights/n" as url from where it gets the flight_id = n, where n is an integer obv.
+    path("<int:flight_id>/book", views.book, name="book"),
+
 ]
