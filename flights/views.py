d8c075d75e95cf7da8fa9bf232226e3b40001a01
irohanrajput <itusharsingh27@gmail.com>
2023-09-27 03:18:50 +0530

    added some more sections to the site like anchor tags etc

--- flights/views.py
+++ flights/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Flight
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def index(request):
     return render(request, "flights/index.html",{"flights": Flight.objects.all()
@@ -8,4 +10,8 @@ def index(request):
 
 def flight(request, flight_id):
     flight = Flight.objects.get(id = flight_id)
-    return render(request, "flights/flight.html", {"flight": flight, "passengers": flight.passengers.all()})
+    return render(request, "flights/flight.html", {
+        "flight": flight, 
+        "passengers": flight.passengers.all()
+        })
+
