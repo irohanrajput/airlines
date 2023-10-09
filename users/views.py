ebafb3c428b454e66ed1b913f755cf26156bebc9
irohanrajput <itusharsingh27@gmail.com>
2023-10-09 23:43:08 +0530

    added instructions to send prompt to various pages, added new pages and completed the authentication functionality, will be adding more features for registered users in upcoming days

--- users/views.py
+++ users/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
 
-def index(request):
+def index_users(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
     return render(request, "users/user.html")
@@ -17,12 +16,18 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request,user)
-            return HttpResponseRedirect(reverse("index"))
+            return HttpResponseRedirect(reverse("index_users"))
         else:
             return render(request, "users/login.html", {"message": "invalid credentidals"})
     return render(request, "users/login.html")
 
 def logout_view(request):
     logout(request)
-    return render(request, "users/login.html", {"message": "Logged out"
+    return render(request, "users/logout.html", {"message": "Logged out Successfully"
                                                 })
+
+def signup_view(request):
+    return render(request, "users/signup.html")
+
+def flight_redr_view(requset):
+    return HttpResponseRedirect(reverse("index"))
