ab9929568dcb9bf68fd8b102589f70b7bddb12a7
irohanrajput <itusharsingh27@gmail.com>
2023-10-09 23:44:11 +0530

    project completed, for now.

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index_users, name="index_users"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
+    path("signup/", views.signup_view, name="signup"),
+    path("flight_redr/", views.flight_redr_view, name="flight_redr"),
 ]
